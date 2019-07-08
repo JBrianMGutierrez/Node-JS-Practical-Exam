a07b39819a16788776125b8446a49062ec5b001e
John Brian M Gutierrez <jinxmugen@gmail.com>
2019-07-08 12:55:09 +0800

    Created view
    created view folder for CRUD function

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 let express = require('express');
-
+let path = require('path');
 let mysql = require('mysql');
 
 let app = express();
 let indexRouter = require('./routes/index');
 let PORT = '3000';
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 let db = mysql.createConnection({
     host: 'localhost',
